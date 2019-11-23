6de7007b315bd77fb35c78d6a436e1c61185d42b
Giovanni Gaffe <gaffejonathan@ymail.com>
2019-11-23 20:57:35 +0800

    Add of switch sender.tag to setup patern to the selected shape

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -9,15 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var paternButtons: [UIButton]!
+    
+    
+    @IBOutlet var paternViews: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func paternButtonTapped(_ sender: UIButton) {
-        print(sender.tag)
+        switch sender.tag {
+        case 1:
+            resetPattern()
+            paternViews[1].isHidden = true
+        case 2:
+            resetPattern()
+            paternViews[2].isHidden = true
+        case 3:
+            resetPattern()
+            
+        default:
+            break
+        }
+    }
+    
+    /// Set all View to normal view
+    func resetPattern() {
+        for i in 0..<paternViews.count {
+            paternViews[i].isHidden = false
+        }
     }
     
 }
---
+++ Instagrid/View/PaternView.swift
@@ -0,0 +1,15 @@
+//
+//  PaternView.swift
+//  Instagrid
+//
+//  Created by mac on 2019/11/23.
+//  Copyright © 2019 Giovanni Gaffé. All rights reserved.
+//
+
+import UIKit
+
+class PaternView: UIView {
+
+  
+
+}
